b2deb3442df21c77ce4ad8122d2f9356d4c56d6c
Guilherme C Gonzalez <gcgonzalez99@hotmail.com>
2021-06-28 22:14:16 -0300

    funcionamento home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,10 @@
 import { GetStaticProps } from 'next';
+import Prismic from '@prismicio/client';
+import Link from 'next/link';
+import { FiCalendar, FiUser } from 'react-icons/fi';
 
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
@@ -24,13 +29,71 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ postsPagination }: HomeProps): JSX.Element {
+  return (
+    <main>
+      <div>
+        {postsPagination.results.map(post => (
+          <Link key={post.uid} href={`/posts/${post.uid}`}>
+            <a>
+              <strong>{post.data.title}</strong>
+              <p>{post.data.subtitle}</p>
+              <div>
+                <time>
+                  <FiCalendar />{' '}
+                  {format(
+                    Date.parse(post.first_publication_date),
+                    'd MMM de yyyy',
+                    {
+                      locale: ptBR,
+                    }
+                  )}
+                </time>
+                <p>
+                  <FiUser /> {post.data.author}
+                </p>
+              </div>
+            </a>
+          </Link>
+        ))}
+      </div>
+      {postsPagination.next_page && <div>Carregar mais posts</div>}
+    </main>
+  );
+}
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
 
-//   // TODO
-// };
+  const postsResponse = await prismic.query(
+    [Prismic.predicates.at('document.type', 'pos')],
+    {
+      fetch: [
+        'pos.title',
+        'pos.subtitle',
+        'pos.author',
+        'pos.banner',
+        'pos.content',
+      ],
+      pageSize: 100,
+    }
+  );
+
+  const posts = postsResponse.results.map(post => {
+    return {
+      uid: post.uid,
+      first_publication_date: post.first_publication_date,
+      data: {
+        title: post.data.title,
+        subtitle: post.data.subtitle,
+        author: post.data.author,
+      },
+    };
+  });
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,7 +1,9 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
-
+import { FiCalendar, FiUser, FiClock } from 'react-icons/fi';
+import { RichText } from 'prismic-dom';
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 import { getPrismicClient } from '../../services/prismic';
-
 import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
 
@@ -26,20 +28,45 @@ interface PostProps {
   post: Post;
 }
 
-// export default function Post() {
-//   // TODO
-// }
+export default function Post() {
+  // TODO
+}
 
-// export const getStaticPaths = async () => {
-//   const prismic = getPrismicClient();
-//   const posts = await prismic.query(TODO);
+export const getStaticPaths = async () => {
+  const prismic = getPrismicClient();
+  const posts = await prismic.query(TODO);
 
-//   // TODO
-// };
+  // TODO
+};
 
-// export const getStaticProps = async context => {
-//   const prismic = getPrismicClient();
-//   const response = await prismic.getByUID(TODO);
+export const getStaticProps: GetStaticProps = async context => {
+  const prismic = getPrismicClient();
+  const response = await prismic.getByUID('posts', String(context), {});
 
-//   // TODO
-// };
+  const post = {
+    slug: context,
+    first_publication_date: response.first_publication_date,
+    data: {
+      title: RichText.asText(response.data.title),
+      banner: response.data.banner,
+      author: RichText.asText(response.data.author),
+      content: response.data.content.map(content => {
+        return {
+          heading: RichText.asText(content.heading),
+          body: content.body.map(postBody => {
+            return {
+              text: RichText.asHtml(postBody.text),
+            };
+          }),
+        };
+      }),
+    },
+  };
+
+  return {
+    props: {
+      post,
+    },
+    redirect: 60 * 30, // 30 minutes
+  };
+};
